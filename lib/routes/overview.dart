097c709c6ec3d00e75306c292815c097ebfd12ed
malloudev <malloudev@gmail.com>
2025-06-23 09:11:55 +0200

    Transplanted previously written code to the OverviewRoute

--- lib/routes/overview.dart
+++ lib/routes/overview.dart
@@ -14,6 +14,8 @@ class OverviewRoute extends StatefulWidget {
 }
 
 class _OverviewState extends State<OverviewRoute> {
+  PageController _pageController = PageController();
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String _versionDetails = '';
   int _currentIndex = 0;
 
@@ -43,18 +45,82 @@ class _OverviewState extends State<OverviewRoute> {
           ])
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.home),
-            label: AppLocalizations.of(context)!.homepageLabel
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.school_rounded),
-            label: AppLocalizations.of(context)!.practiceLabel
-          )
-        ]
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          highlightColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          splashFactory: NoSplash.splashFactory,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: Theme.of(context).colorScheme,
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.home),
+              label: AppLocalizations.of(context)!.homepageLabel
+            ),
+            BottomNavigationBarItem(
+              icon: const Icon(Icons.school_rounded),
+              label: AppLocalizations.of(context)!.practiceLabel
+            ),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.account_tree_rounded),
+                label: AppLocalizations.of(context)!.coursesLabel
+            ),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.folder_special_rounded),
+                label: AppLocalizations.of(context)!.filesLabel
+            )
+          ],
+          onTap: (int index) {
+            _currentIndex = index;
+          },
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: PageView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _pageController,
+                  children: [
+                    HomePage(),
+
+                  ].map((Widget element) => Padding(
+                    padding: EdgeInsets.only(top: 48.0, bottom: 12.0),
+                    child: element,
+                  )).toList() as List<Widget>
+              ),
+            ),
+            Positioned(
+                top: 8.0,
+                right: 8.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    IconButton(icon: const Icon(Icons.settings), iconSize: 28.0, onPressed: () {
+                      setState(() => _pageController.animateToPage(_currentIndex = 0, duration: Duration(microseconds: 1), curve: Curves.linear));
+                      Navigator.of(context).pushNamed('/settings');
+                    }),
+                    IconButton(icon: const Icon(Icons.account_circle), iconSize: 28.0, onPressed: () {
+                      _scaffoldKey.currentState!.openEndDrawer();
+                    })
+                  ],
+                )
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text(_versionDetails, style: const TextStyle(
+                fontSize: 12.0,
+                color: Color.fromARGB(255, 66, 66, 66),
+              )),
+            ),
+          ],
+        ),
       )
     );
   }
