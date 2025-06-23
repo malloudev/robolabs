09d573f4930bf43ef055357418a7638e8754cc8b
malloudev <malloudev@gmail.com>
2025-06-23 09:25:04 +0200

    Added Overview Wrapper for the Code Editor

---
+++ lib/overview/editor.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../editor/index.dart';
+
+class EditorWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CodeEditor();
+  }
+}
