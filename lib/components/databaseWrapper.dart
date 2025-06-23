2bc7162430753cd461ca36f7a4f13846017656e6
malloudev <malloudev@gmail.com>
2025-06-23 08:39:03 +0200

    Abstracted the Database outside the main function

---
+++ lib/components/databaseWrapper.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+abstract final class DatabaseWrapper {
+  static late Database _db;
+
+  static void ensureInitialized() async {
+    _db = await openDatabase(
+      join(await getDatabasesPath(), 'robolabs.db'),
+      version: 1,
+      onCreate: (Database db, int version) async {
+        final batch = db.batch();
+
+        batch.execute('CREATE TABLE IF NOT EXISTS userdata(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT)');
+        batch.execute('CREATE TABLE IF NOT EXISTS courses(id INTEGER PRIMARY KEY AUTOINCREMENT)');
+        batch.execute('CREATE TABLE IF NOT EXISTS preferences(id INTEGER PRIMARY KEY AUTOINCREMENT)');
+        batch.execute('CREATE TABLE IF NOT EXISTS preferences(id INTEGER PRIMARY KEY AUTOINCREMENT)');
+
+        await batch.commit();
+      }
+    );
+  }
+
+  static void enterUser(String name) {
+    _db.insert('userdata', { 'username': name });
+  }
+}
